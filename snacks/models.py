81201926162fa489b103a2dbf260efb13d7422c0
hayabalasmeh <hmbalasmeh@yahoo.com>
2021-08-30 20:17:14 +0300

    adding django models

--- snacks/models.py
+++ snacks/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-from django.db.models import Model
+from django.db import models
 from django.contrib.auth import get_user_model
 
-class Snack(Model):
+class Snack(models.Model):
     name = models.CharField(max_length= 64)
     purchaser = models.ForeignKey(get_user_model(),on_delete = models.CASCADE)
     description = models.TextField()
--- snacks/tests.py
+++ snacks/tests.py
@@ -1,3 +1,43 @@
+from snacks.models import Snack
 from django.test import TestCase
 
-# Create your tests here.
+from django.urls import reverse
+
+
+class SnacksTest(TestCase):
+    def test_list_page_status(self):
+        #Arrange
+        url = reverse('snack_list')
+        #Act
+        response = self.client.get(url)
+        #Assert
+        self.assertEqual(response.status_code, 200)
+
+    def test_detail_page_status(self):
+        #Arrange
+        url = reverse('snack_detail')
+        #Act
+        response = self.client.get(url)
+        #Assert
+        self.assertEqual(response.status_code, 200)
+
+    def test_list_page_templete(self):
+        #Arrange
+        url = reverse('snack_list')
+        #Act
+        response = self.client.get(url)
+        #Assert
+        self.assertTemplateUsed(response, 'snack_list.html')
+        self.assertTemplateUsed(response, 'base.html')
+
+    def test_about_page_templete(self):
+        #Arrange
+        url = reverse('snack_detail')
+        #Act
+        response = self.client.get(url)
+        #Assert
+        self.assertTemplateUsed(response, 'snack_detail.html')
+        self.assertTemplateUsed(response, 'base.html')
+
+
+
